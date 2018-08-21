c85d0f9529412f7b94d648d3b8758685d8718441
yanqiangmiffy <1185918903@qq.com>
2018-08-21 16:20:17 +0800

    数据库更新

--- demo.py
+++ demo.py
@@ -1,8 +1,16 @@
-from pymongo import MongoClient
+# from py2neo import Graph,Node
+# graph=Graph(
+#     host="127.0.0.1",
+#     port=7687, # 可写可不写  bolt port
+#     user="neo4j",
+#     password="12345"
+# )
+#
+# qiang=Node("Person",name="闫强",age=24)
+# xiong=Node("Person",name="闫强",age=24)
+#
+# ming=Node("Drink",name="小明",calories=90)
+# hong=Node("Drink",name="小红",calories=0)
+#
+# graph.create(qiang|xiong|ming|hong)
 
-conn=MongoClient('127.0.0.1',27017)
-
-db=conn.game # 连接game数据库，没有则自动创建
-my_set=db.game_info # 使用game_info集合，没有则自动创建
-
-my_set.insert({'game_id': 0, 'game_name': '暗黑地牢', 'game_rating': 5, 'game_url': 'http://down.ali213.net/pcgame/darkestdungeon.html', 'cn_title': '《暗黑地牢》免安装简体中文绿色版[Ancestral版|Build 24353|官方中文]', 'en_title': 'Darkest Dungeon', 'pic_url': 'http://imgs.ali213.net/oday/uploadfile/2015/01/05/2015010530506624.jpg', 'game_category': '游戏类型：策略战棋SLG', 'product_designer': '游戏类型：策略战棋SLG', 'release_date': '游戏类型：策略战棋SLG', 'file_size': '游戏类型：策略战棋SLG', 'game_tags': '单人单机;2D画面;不支持手柄;菜鸟入门;RPG;策略', 'game_intro': '《暗黑地牢(DarkestDungeon)》是一款充满地狱色彩的RPG。画风硬朗不凡，颇具美式动漫的风格。爆裂的火花，凶恶的地牢战士，赤红的鲜血……一切都被一把燃烧着希望之光的火把联系起来。在这样充满压抑和恐惧的氛围中，将会发生怎样的故事呢？《暗黑地牢》将采用回合制RPG的方式进行战斗，丰富的人物属性和技能需要玩家更深入的掌控。与ARPG不同，回合制RPG或许在战斗方面更加考验玩家对人物编排，设定，以及属性方面的了解。'})
--- spider/database.py
+++ spider/database.py
@@ -2,26 +2,74 @@
 import os
 from pymongo import MongoClient
 import pandas as pd
+from py2neo import Graph,Node,Relationship,NodeMatcher
+from tqdm import tqdm
 
+# 数据路径
 config={
     'project_dir':os.path.abspath(os.path.join(os.getcwd(), "..")),
     'headers':{
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
     }
 }
-
-conn=MongoClient('127.0.0.1',27017)
-db=conn.game # 连接game数据库，没有则自动创建
-my_set=db.game_info # 使用game_info集合，没有则自动创建
 info_file_path = os.path.join(config['project_dir'], 'data', 'game_info.csv')
-
 data=pd.read_csv(info_file_path)
+data['product_designer']=data['product_designer'].apply(lambda x: x.replace("制作发行：",'').strip())
 keys=data.columns
 
 
 
-for indexs in data.index:
-    row=dict(zip(keys,data.loc[indexs].values[0:-1]))
-    row['game_id']=int(row['game_id'])
-    row['game_rating']=int(row['game_rating'])  # pymongo 不能编码numpy.int64，这里需要强制转化为int类型
-    my_set.insert(row)
+def save_mongodb():
+    """
+    将数据存入到MongoDB
+    :return:
+    """
+    conn=MongoClient('127.0.0.1',27017)
+    db=conn.game # 连接game数据库，没有则自动创建
+    my_set=db.game_info # 使用game_info集合，没有则自动创建
+    print("正在将数据存入MongoDB。。")
+    for i in tqdm(data.index):
+        row=dict(zip(keys,data.loc[i].values[0:-1]))
+        row['game_id']=int(row['game_id'])
+        row['game_rating']=int(row['game_rating'])  # pymongo 不能编码numpy.int64，这里需要强制转化为int类型
+        row['product_designer']=row['product_designer'].replace("制作发行：",'').strip()  # pymongo 不能编码numpy.int64，这里需要强制转化为int类型
+        my_set.insert(row)
+
+
+def save_neo4j():
+    graph=Graph(
+        host="127.0.0.1",
+        user="neo4j",
+        password="12345"
+    )
+
+    print("正在创建游戏发行商节点。。")
+    # 创建发行商节点
+    productor_names=set(data['product_designer'].tolist())
+    for productor_name in tqdm(productor_names):
+        productor_node=Node("Productor",name=productor_name)
+        graph.create(productor_node)
+
+    # 创建游戏节点以及与发行商的关系
+    print("正在创建游戏节点以及与发行商的关系。。")
+    matcher=NodeMatcher(graph)
+    for i in tqdm(data.index):
+        row = dict(zip(keys, data.loc[i].values[0:-1]))
+        row['game_id'] = int(row['game_id'])
+        row['game_rating'] = int(row['game_rating'])
+
+        # 游戏节点
+        productor_name=row.pop('product_designer')
+        productor_node=matcher.match("Productor", name=productor_name).first()
+
+        game_node=Node("Game")
+        game_node.update(row)
+
+        # 建立关系
+        relation=Relationship(productor_node,"发行",game_node)
+        node_re=productor_node|game_node|relation
+
+        graph.create(node_re)
+
+
+save_mongodb()
